0abf0331f56e7beca126f29461e15c3bfe524602
macypa@gmail.com <st17chstitch>
2017-01-18 17:49:36 +0200

    load lazy images and video only after div is expanded

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,41 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 import socket from "./socketTorr"
+
+window.toggle_visibility = function(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else {
+      e.style.display = 'block';
+      deferImages(e);
+   }
+}
+
+window.deferImages = function(container, imgUrl) {
+  var imgDefer = container.getElementsByClassName('lazy');
+  for (var i=0; i<imgDefer.length; i++) {
+    if(imgDefer[i].getAttribute('data-src')) {
+//      imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+      preloadImg(imgDefer[i])
+    }
+  }
+}
+
+window.preloadImg = function(imgElem) {
+	setTimeout(function() {
+		// XHR to request a JS and a CSS
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            imgElem.setAttribute('src', imgElem.getAttribute('data-src'));
+        }
+    }
+//		xhr.responseType = "blob";
+		xhr.open('GET', imgElem.getAttribute('data-src'));
+    xhr.setRequestHeader("Referer", "http://zamunda.net/");
+		xhr.send('');
+		// preload image
+		new Image().src = imgElem.getAttribute('data-src');
+	}, 1000);
+};
