e3405f1ef7e23fd97e79bc6f8a4e51363ee95e93
macypa@gmail.com <st17chstitch>
2017-01-20 16:08:11 +0200

    lightbox for images

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -65,3 +65,42 @@ window.preloadImg = function(imgElem) {
 		new Image().src = imgElem.getAttribute('data-src');
 	}, 1000);
 };
+
+
+
+window.openModal = function(idModal) {
+  document.getElementById(idModal).style.display = "block";
+}
+
+window.closeModal = function(idModal) {
+  document.getElementById(idModal).style.display = "none";
+}
+
+var slideIndex = 1;
+
+window.plusSlides = function(idModal, n) {
+  window.showSlides(idModal, slideIndex += n);
+}
+
+window.currentSlide = function(idModal, n) {
+  window.showSlides(idModal, slideIndex = n);
+}
+
+window.showSlides = function(idModal, n) {
+  var i;
+  var idModalElem = document.getElementById(idModal);
+  var slides = idModalElem.getElementsByClassName("mySlides");
+  var dots = idModalElem.getElementsByClassName("lightbox");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
