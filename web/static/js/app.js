3571aa38c8bec9f2d6c75e18bfa0a89e05c72326
Shlyakls Dattebayo <macypa@gmail.com>
2017-01-21 13:16:30 +0200

    lightbox image viewer close expand

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -34,37 +34,10 @@ window.deferImages = function(container, imgUrl) {
   var imgDefer = container.getElementsByClassName('lazy');
   for (var i=0; i<imgDefer.length; i++) {
     if(imgDefer[i].getAttribute('data-src')) {
-//      imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-      preloadImg(imgDefer[i])
+      imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
     }
   }
 }
-
-window.preloadImg = function(imgElem) {
-	setTimeout(function() {
-		// XHR to request a JS and a CSS
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            imgElem.setAttribute('src', imgElem.getAttribute('data-src'));
-        }
-    }
-//		xhr.responseType = "blob";
-		xhr.open('GET', imgElem.getAttribute('data-src'));
-    xhr.setRequestHeader("Referer", imgElem.getAttribute('data-src').replace(/[^/]+$/i, ""));
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xhr.setRequestHeader("Accept-Encoding", "gzip;deflate,sdch");
-    xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.8");
-    xhr.setRequestHeader("Upgrade-Insecure-Requests", "1");
-    xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36");
-    xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-//    xhr.setRequestHeader("Cookie", tracker.cookie);
-    xhr.setRequestHeader("Connection", "keep-alive");
-		xhr.send('');
-		// preload image
-		new Image().src = imgElem.getAttribute('data-src');
-	}, 1000);
-};
 
 
 
