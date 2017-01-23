84ccadae5471bba77c338f663cde44a42046e573
Shlyakls Dattebayo <macypa@gmail.com>
2017-01-23 19:20:05 +0200

    download images with headers

--- web/helpers/image_downloader.ex
+++ web/helpers/image_downloader.ex
@@ -3,21 +3,36 @@ defmodule Torr.ImageDownloader do
 
   def download(torrents) do
     torrents |> Enum.each(fn torrent ->
-                   unless is_nil(torrent.json["images"]) or torrent.json["images"] == [] do
-                     for img <- torrent.json["images"] do
-                       image = img |> String.trim |> String.replace("http://", "") |> String.replace("http://", "")
-                       downloadImage(image)
-                     end
-                   end
+                   downloadList(torrent.json["images"])
+                   downloadList(torrent.json["imagesHidden"])
                  end)
   end
 
+  def downloadList(images) do
+    unless is_nil(images) or images == [] do
+      for img <- images do
+        image = img |> String.trim |> String.replace("http://", "") |> String.replace("http://", "")
+        downloadImage(image)
+      end
+    end
+  end
+
   def downloadImage(url) do
-      options = [hackney: [{:follow_redirect, true}], timeout: :infinity, recv_timeout: :infinity]
-      body = case HTTPoison.get(url, [], options) do
+      headers = [
+                "Accept-Encoding": "gzip;deflate,sdch",
+                "Accept-Language": "en-US,en;q=0.8",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Referer": "http://zamunda.net",
+#                "Cookie": tracker.cookie,
+                "Connection": "keep-alive"
+              ]
+      options = [connect_timeout: 1000000, timeout: 1000000, recv_timeout: 1000000, hackney: [{:follow_redirect, true}]]
+      body = case HTTPoison.get(url, headers, options) do
           {:ok, %HTTPoison.Response{body: body, headers: _headers, status_code: 200}} ->
              body
-          other -> Logger.info "img error: #{inspect(other)}"
+          other -> Logger.info "img #{url} error: #{inspect(other)}"
       end
 
       url = url |> String.replace("http://", "") |> String.replace("http://", "") |> URI.decode()
